b5302ae8b09bcd254821d04cdb2b71ace6e8a64f
imwiwiim <wilsondaniel95@hotmail.com>
2017-02-20 12:51:27 -0500

    finished test pwm

--- test_pwm.py
+++ test_pwm.py
@@ -1,9 +1,28 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
+import sys
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.output(13,1)
+p = GPIO.PWM(12)
+p.start(100)
+while True:
+	try:
+		print "[1] Change Frequency"
+		print "[2] Change Duty Cycle"
+		print "[q] quit"
+		i = raw_input('Insert option: ')   # use raw_input for Python 2
+		if i == "q":
+			break
+		if i == "2":
+			pwm = float(raw_input('DUTY CYCLE 0.0 - 100.0: '))
+			p.ChangeDutyCycle(pwm)
+		if i == "1":
+			frq = float(raw_input('Frequency: '))
+			p.ChangeFrequency(frq)
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
+		break
 
-p = GPIO.PWM(12, 0.5)
-p.start(1)
-raw_input('Press return to stop:')   # use raw_input for Python 2
 p.stop()
 GPIO.cleanup()
