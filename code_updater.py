ed0380a65f16c633924b034aadfc50609d936080
imwiwim90 <wilsondaniel95@hotmail.com>
2017-03-05 19:17:56 -0500

    time thresh

--- code_updater.py
+++ code_updater.py
@@ -1,8 +1,7 @@
 import subprocess
 import time
-
+time.sleep(20)
 subprocess.call(['git','reset','--hard'])
 subprocess.call(['git','pull','origin','master'])
 subprocess.call(['mkdir','tetadas'])
-time.sleep(10)
 print "finished"
