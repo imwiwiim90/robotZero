d67be931983a60e2dc4ed8dcf962d28999ef8ec8
imwiwim90 <wilsondaniel95@hotmail.com>
2017-03-05 20:01:52 -0500

    fixed erorr

--- code_updater.py
+++ code_updater.py
@@ -2,13 +2,14 @@ import subprocess
 import time
 ps = subprocess.Popen('ls | grep ref.willy',shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 output = ps.communicate()[0]
+
 if output == 'ref.willy\n':
-	pass
-else:
 	time.sleep(20)
 	subprocess.call(['git','reset','--hard'])
 	subprocess.call(['git','pull','origin','master'])
-	subprocess.call(['touch','ref.willy'])
 	subprocess.call(['python','robot/main.py'])
 	subprocess.call(['rm','ref.willy'])
-	print "finished"
+else:
+	pass
+
+print "finished"
