305b0e98136973d08ddb3ce53eb67be33b3eb607
imwiwiim90 <wilsondaniel95@hotmail.com>
2017-04-01 00:25:33 -0500

    kddkdkd

--- distanceSensor.py
+++ distanceSensor.py
@@ -3,7 +3,7 @@ import time
 import threading
 
 class SDistance(threading.Thread):
-	def __init__(self,trig_pin,echo_pin):
+	def __init__(self,trig_pin,echo_pin,N = 5):
 		threading.Thread.__init__(self)
 		GPIO.setmode(GPIO.BCM)
 		self.echo = echo_pin
@@ -14,12 +14,15 @@ class SDistance(threading.Thread):
 		GPIO.output(trig_pin,0)
 		time.sleep(0.5)
 		self.distance = 0
+		self.data = []
+		self.N
 
 
 	def run(self):
 		TRIG = self.trig
 		ECHO = self.echo
 		while True:
+			time.sleep(1/60.0)
 			GPIO.output(TRIG,1)
 			time.sleep(0.000001)
 			GPIO.output(TRIG,False)
@@ -29,8 +32,13 @@ class SDistance(threading.Thread):
 			ans = GPIO.wait_for_edge(ECHO, GPIO.FALLING, timeout=int(100*1000/(17150)))
 			pulse = time.time() - pulse_start
 			distance = pulse*17150
+			if len(self.data) >= self.N:
+				l = self.data.pop()
+				self.distance -= l
+			self.distance += distance/float(self.N)
+			self.data.append(distance/float(self.N))
 
-			self.distance = distance
+
 
 
 
