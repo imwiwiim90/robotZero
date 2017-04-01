c52a5e63468c3e89fccddfc6e5ab3fde0fb6fd93
imwiwiim90 <wilsondaniel95@hotmail.com>
2017-03-31 23:43:52 -0500

    pum pum

--- test_sensor3.py
+++ test_sensor3.py
@@ -2,9 +2,10 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)  
 TRIG = 5
 ECHO = 6
+GPIO.setup(TRIG,GPIO.OUT)
 # GPIO 23 set up as input. It is pulled up to stop false signals  
 GPIO.setup(ECHO, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-
+GPIO.output(TRIG,0)
 print "Make sure you have a button connected so that when pressed"  
 print "it will connect GPIO port 23 (pin 16) to GND (pin 6)\n"  
 raw_input("Press Enter when ready\n>")  
@@ -13,12 +14,12 @@ print "Waiting for falling edge on port 23"
 # now the program will do nothing until the signal on port 23   
 # starts to fall towards zero. This is why we used the pullup  
 # to keep the signal high and prevent a false interrupt  
-  
-print "During this waiting time, your computer is not"   
-print "wasting resources by polling for a button press.\n"  
-print "Press your button when ready to initiate a falling edge interrupt."  
+GPIO.output(TRIG,1)
+time.sleep(0.00001)
+GPIO.output(TRIG,False)
+
 try:  
-    GPIO.wait_for_edge(23, GPIO.FALLING)  
+    GPIO.wait_for_edge(ECHO, GPIO.FALLING)  
     print "\nFalling edge detected. Now your program can continue with"  
     print "whatever was waiting for a button press."  
 except KeyboardInterrupt:  
