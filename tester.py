1bc733259f2f8f4984722c5b48af1c3070b30442
imwiwiim90 <wilsondaniel95@hotmail.com>
2017-04-03 19:29:57 -0500

    sss

--- tester.py
+++ tester.py
@@ -2,10 +2,10 @@ import subprocess
 import time
 ps = subprocess.Popen("ps auc| grep python | wc -l ",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 n = int(ps.communicate(0)[0].strip())
-ps = subprocess.Popen("ps auc| grep python | awk '{print $2}'| sort  | head -n" + str(n-1) + "| xargs kill ",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-#subprocess.Popen("git reset --hard",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-#ps = subprocess.Popen("git pull origin master",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-#subprocess.Popen("sudo python robot/main.py",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+ps = subprocess.Popen("ps auc| grep python | awk '{print $2}'| sort  | head -n" + str(n-1) + "| sudo xargs kill ",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+subprocess.Popen("git reset --hard",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+ps = subprocess.Popen("git pull origin master",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+subprocess.Popen("sudo python robot/main.py",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 
 
 #time.sleep(10)
