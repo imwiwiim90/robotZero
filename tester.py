5ce3099e9e3a1f2368b571d73feab5c35de22a9e
imwiwiim90 <wilsondaniel95@hotmail.com>
2017-04-05 11:37:06 -0500

    updating code

--- tester.py
+++ tester.py
@@ -3,9 +3,13 @@ import time
 ps = subprocess.Popen("ps auc| grep python | wc -l ",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 n = int(ps.communicate(0)[0].strip())
 ps = subprocess.Popen("ps auc| grep python | awk '{print $2}'| sort  | head -n" + str(n-1) + "| sudo xargs kill ",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+print ps.communicate(0)
 subprocess.Popen("git reset --hard",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+print ps.communicate(0)
 ps = subprocess.Popen("git pull origin master",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-subprocess.Popen("sudo python robot/main.py",shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+ps.communicate(0)[0]
+f = open("error.txt","w")
+subprocess.Popen("sudo python robot/main.py",shell=True,stderr=f)
 
 
 #time.sleep(10)
