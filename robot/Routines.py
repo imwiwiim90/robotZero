d2e6cbbaadc7cdad6c86f1c58a6a1f5c46e9e76b
imwiwiim90 <wilsondaniel95@hotmail.com>
2017-04-07 17:18:07 -0500

    sss

--- robot/Routines.py
+++ robot/Routines.py
@@ -9,7 +9,7 @@ class Seesaw(threading.Thread):
 
     def run(self):
         agent = self.agent
-        straight_time = 1.5
+        straight_time = 1.6
         time_start  = time.time()
         speed_aux   = agent.speed
         agent.speed = 100
@@ -51,7 +51,7 @@ class ReleaseBolt(threading.Thread):
 
     def run(self):
         agent = self.agent
-        straight_time = 0.16
+        straight_time = 0.12
         time_start  = time.time()
         speed_aux   = agent.speed
         agent.speed = 100
